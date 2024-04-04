24e7a6eb66c89f3b5ba8159236ac6576221ad045
Mazidulla <mazidula.mk@gmail.com>
2024-04-04 15:03:10 +0530

    Adding changes

--- vars/scripts.groovy
+++ vars/scripts.groovy
@@ -7,10 +7,6 @@ def call() {
     def classLoader = this.class.classLoader
     def configStream = classLoader.getResourceAsStream("config.properties")
 
-    if (configStream != null) {
-        configProperties.load(configStream)
-        return "Hello from shared library! Config key value: ${configProperties.getProperty('key')}"
-    } else {
-        return "Hello from shared library! (config.properties not found)"
-    }
+    configProperties.load(configStream)
+    def channel=configProperties.getProperty('SLACK_CHANNEL_NAME')
 }
