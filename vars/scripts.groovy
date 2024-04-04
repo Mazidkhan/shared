2f8a06125c59be0e7a2c9ddc884d9f53cf154168
Mazidkhan <mazidula.mk@gmail.com>
2024-04-04 13:59:07 +0530

    Update scripts.groovy

--- vars/scripts.groovy
+++ vars/scripts.groovy
@@ -1,3 +1,11 @@
-def call(Map config = [:]){
-    sh "echo ${config.name} ${config.lname}"
+import groovy.yaml.Yaml
+
+def call(Map config = [:]) {
+    def yamlContent = readFile(config.yamlFile)
+    def yamlData = new Yaml().load(yamlContent)
+    
+    def name = yamlData.firstname.trim()
+    def lname = yamlData.lname.trim()
+    
+    sh "echo ${name} ${lname}"
 }
